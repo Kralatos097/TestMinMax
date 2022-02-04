407b826fdf51a70fecb7537fe851a569b012e622
Thibaud CHEVILLARD <thibaud097@gmail.com>
2022-02-04 11:16:28 +0100

    Ajout NegaMax et élagage AlphaBeta

--- Assets/Scripts/AIBrain.cs
+++ Assets/Scripts/AIBrain.cs
@@ -24,7 +24,12 @@ public class AIBrain {
         foreach (Piece availablePiece in Board.AvailablePieces(Player)) {
             foreach (Coordinate availableMove in availablePiece.AvailableMoves(Board)) {
                 Node node = new Node(Board, Player, Player, availablePiece.CurrentCoordinate, availableMove);
-                int value = MinMax(node, DepthSearch, false);
+                
+                //int value = MinMax(node, DepthSearch, false);
+                //int value = NegaMax(node, DepthSearch, 1);
+                //int value = AlphaBeta(node, DepthSearch, false, -100000000, 100000000);
+                int value = NegaAlphaBeta(node, DepthSearch, false, -10000000);
+                
                 Nodes.Add(new Tuple<int, Node>(value, node));
             }
         }
@@ -53,7 +58,7 @@ public class AIBrain {
 
             foreach (Node nodeChild in node.Children)
             {
-                Mathf.Max(value, MinMax(nodeChild, depth-1, false));
+                value = Mathf.Max(value, MinMax(nodeChild, depth-1, false));
             }
         }
         else
@@ -61,10 +66,84 @@ public class AIBrain {
             value = 1000000000;
             foreach (Node nodeChild in node.Children)
             {
-                Mathf.Max(value, MinMax(nodeChild, depth-1, true));
+                value = Mathf.Min(value, MinMax(nodeChild, depth-1, true));
             }
         }
         return value;
     }
     
+    private int NegaMax(Node node, int depth, int isMax)
+    {
+        if (depth <= 0 || node.IsTerminal)
+        {
+            return isMax * node.HeuristicValue;
+        }
+
+        int value = -1000000000;
+
+        foreach (Node nodeChild in node.Children)
+        {
+            value = Mathf.Max(value, -NegaMax(nodeChild, depth-1, -isMax));
+        }
+        return value;
+    }
+    
+    private int AlphaBeta(Node node, int depth, bool isMax, int alpha, int beta)
+    {
+        if (depth <= 0 || node.IsTerminal)
+        {
+            return node.HeuristicValue;
+        }
+
+        int value;
+        if (isMax)
+        {
+            value = -1000000000;
+
+            foreach (Node nodeChild in node.Children)
+            {
+                value = Mathf.Max(value, AlphaBeta(nodeChild, depth-1, true, alpha, beta));
+                if (value >= beta)
+                {
+                    return value;
+                }
+
+                alpha = Mathf.Max(alpha, value);
+            }
+        }
+        else
+        {
+            value = 1000000000;
+            foreach (Node nodeChild in node.Children)
+            {
+                value = Mathf.Min(value, AlphaBeta(nodeChild, depth-1, true, alpha, beta));
+                if (value <= alpha)
+                {
+                    return value;
+                }
+
+                beta = Mathf.Min(beta, value);
+            }
+        }
+        return value;
+    }
+    
+    private int NegaAlphaBeta(Node node, int depth, bool isMax, int alpha)
+    {
+        if (depth <= 0 || node.IsTerminal)
+        {
+            return node.HeuristicValue;
+        }
+
+        int value = -1000000000;
+        foreach (Node nodeChild in node.Children)
+        {
+            value = Mathf.Max(value, NegaAlphaBeta(nodeChild, depth-1, true, alpha));
+            if (-value <= alpha)
+            {
+                return -value;
+            }
+        }
+        return -value;
+    }
 }
